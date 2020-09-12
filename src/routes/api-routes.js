5cd4596bcee77d493ccee5f0724efb285edfdf5f
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-12 14:03:09 +0100

    Get all plans sends back a json object with the data from the database

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import db from '../models';
 
 const router = express.Router();
 
@@ -9,14 +10,15 @@ const getXibit = (req, res) => {
   res.send('Get 1 xibit');
 };
 
-const getAllPlans = (req, res) => {
-  res.send('Get All');
-};
-const updatePlan = (req, res) => {
-  res.send('Update one');
+const getAllPlans = async (req, res) => {
+  const data = await db.Plan.find({});
+  res.json(data);
 };
 const addPlans = (req, res) => {
   res.send('Add Plans');
+};
+const updatePlan = (req, res) => {
+  res.send('Update one');
 };
 const deletePlans = (req, res) => {
   res.send('Delete Plans');
@@ -26,8 +28,8 @@ router.post('/xibits', getXibits);
 router.get('/xibits/:id', getXibit);
 
 router.get('/plans', getAllPlans);
-router.put('/plans/:id', updatePlan);
 router.post('/plans', addPlans);
+router.put('/plans/:id', updatePlan);
 router.delete('/plans/:id', deletePlans);
 
 export default router;
