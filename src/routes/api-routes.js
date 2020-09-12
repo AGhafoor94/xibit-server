d5820e4d6daf3592d9057682782af5b7eb536d5f
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-12 14:00:21 +0100

    Added the 4 CRUD routes for the plans

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -9,7 +9,25 @@ const getXibit = (req, res) => {
   res.send('Get 1 xibit');
 };
 
+const getAllPlans = (req, res) => {
+  res.send('Get All');
+};
+const updatePlan = (req, res) => {
+  res.send('Update one');
+};
+const addPlans = (req, res) => {
+  res.send('Add Plans');
+};
+const deletePlans = (req, res) => {
+  res.send('Delete Plans');
+};
+
 router.post('/xibits', getXibits);
 router.get('/xibits/:id', getXibit);
+
+router.get('/plans', getAllPlans);
+router.put('/plans/:id', updatePlan);
+router.post('/plans', addPlans);
+router.delete('/plans/:id', deletePlans);
 
 export default router;
