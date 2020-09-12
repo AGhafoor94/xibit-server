a80ebc5df112e3c08786b372376fd5397a932360
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-12 14:18:03 +0100

    Find by id working but it needs the id of the plan in the database. Will need to change to something like id 1

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -10,16 +10,33 @@ const getXibit = (req, res) => {
   res.send('Get 1 xibit');
 };
 
-const getAllPlans = async (req, res) => {
-  const data = await db.Plan.find({});
-  res.json(data);
+const getAllPlans = async (_, res) => {
+  try {
+    const data = await db.Plan.find({});
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
+
+const getPlanById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await db.Plan.findById(id);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
 const addPlans = (req, res) => {
   res.send('Add Plans');
 };
+
 const updatePlan = (req, res) => {
   res.send('Update one');
 };
+
 const deletePlans = (req, res) => {
   res.send('Delete Plans');
 };
@@ -28,6 +45,7 @@ router.post('/xibits', getXibits);
 router.get('/xibits/:id', getXibit);
 
 router.get('/plans', getAllPlans);
+router.get('/plans/:id', getPlanById);
 router.post('/plans', addPlans);
 router.put('/plans/:id', updatePlan);
 router.delete('/plans/:id', deletePlans);
