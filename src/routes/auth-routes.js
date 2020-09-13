fe0b65b767f4a1b28dc97908a556173eb91b733a
Adnan <adnan_ghafoor94@hotmail.com>
2020-09-13 12:10:57 +0100

    Added if user already exists authentication

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -6,30 +6,34 @@ import db from '../models';
 const router = express.Router();
 
 const validate = (body) => {
-  const {
-    firstName, lastName, email, password,
-  } = body;
+  const { firstName, lastName, email, password } = body;
   const isValid = firstName && lastName && email && password;
   return isValid;
 };
 
 const registerUsers = async (req, res) => {
-  const {
-    firstName, lastName, email, password,
-  } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
   if (validate(req.body)) {
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds);
-    await db.User.create({
-      firstName: firstName.trim(),
-      lastName: lastName.trim(),
-      email,
-      password: passwordHash,
-    });
-    res
-      .status(201)
-      .json({ success: true, message: `Account created for ${email}` });
+    const user = await db.User.findOne({ email });
+    if (user) {
+      res.send(303).json({
+        success: false,
+        message: 'User already exists',
+      });
+    } else {
+      const saltRounds = 10;
+      const passwordHash = await bcrypt.hash(password, saltRounds);
+      await db.User.create({
+        firstName: firstName.trim(),
+        lastName: lastName.trim(),
+        email,
+        password: passwordHash,
+      });
+      res
+        .status(201)
+        .json({ success: true, message: `Account created for ${email}` });
+    }
   } else {
     res.send(400).json({
       success: false,
@@ -38,6 +42,8 @@ const registerUsers = async (req, res) => {
   }
 };
 
-router.get('/register', registerUsers);
+const loginUser = (req, res) => {};
 
+router.get('/register', registerUsers);
+router.post('/login', loginUser);
 export default router;
